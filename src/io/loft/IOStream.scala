3d1842ab1356679c2103fc3aca75306a25005960
rschildmeijer <schildmeijer@gmail.com>
2011-01-14 23:08:22 +0100

    IOStream cont'd

--- src/io/loft/IOStream.scala
+++ src/io/loft/IOStream.scala
@@ -1,5 +1,6 @@
 package io.loft
 
+import java.nio.channels.SelectionKey
 import java.net.InetSocketAddress
 import java.nio.channels.SocketChannel
 import scala.collection.mutable.MutableList
@@ -11,7 +12,7 @@ class IOStream(channel: SelectableChannel, maxBufferSize: Long = 104857600, read
 
   private val readBuffer = new StringBuilder()
   private val writeBuffer = new StringBuilder()
-  private var readDelimeter = ' '
+  private var readDelimeter = ""
   private var readBytes = 0
 
   private var readCallback: (String) => Unit = null
@@ -28,6 +29,60 @@ class IOStream(channel: SelectableChannel, maxBufferSize: Long = 104857600, read
         this.connectCallback = connectCallback
       }
     }
+  }
+
+  def readUntil(delimeter: String, callback: (String) => Unit) {
+    readCallback = callback
+    readDelimeter = delimeter
+
+    var continue = true
+
+    while (continue) {
+      if (readFromBuffer()) return
+      checkClosed()
+      if (readToBuffer() == 0) continue = false
+    }
+    addIOState(SelectionKey.OP_READ)
+  }
+
+  def readFromBuffer(): Boolean = {
+    if (readBytes != 0 && readBuffer.size >= readBytes) {
+      val numBytes = readBytes
+      val callback = readCallback
+      readCallback = null
+      readBytes = 0
+      runCallback(callback, consume(numBytes))
+      return true
+    } else if (!readDelimeter.isEmpty) {
+      val loc = readBuffer.indexOf(readDelimeter)
+      if (loc != -1) {
+        val callback = readCallback
+        val delimeterLength = readDelimeter.size
+        readCallback = null
+        readDelimeter = ""
+        runCallback(callback, consume(loc + delimeterLength))
+        return true
+      }
+    }
+    false
+  }
+
+  def checkClosed() {
+
+  }
+
+  def readToBuffer(): Int = {
+    2
+  }
+
+  def addIOState(ops: Int) {
+
+  }
+
+  def runCallback(callback: (String) => Unit, data: String) = callback(data)
+
+  def consume(numBytes: Int): String = {
+    ""
   }
 
 }
